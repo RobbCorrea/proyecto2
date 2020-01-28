0ac6870badabb63e4afcd24a2f2fd23cef7cb3dc
Edgar Chavero <edieveg316@gmail.com>
2020-01-28 14:09:02 -0600

    Routes de Login

--- middlewares/LoggeOut.js
+++
@@ -1,7 +0,0 @@
-module.exports = route => (req, res, next) => {
-  if (!req.isAuthenticated()) {
-    next()
-  } else {
-    res.redirect(route)
-  }
-}
---
+++ routes/auth.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const router = express.Router();
+const { toProfile } = require("../config/middlewareAuth");
+const authcontrollers = require("../controllers/authController");
+
+router.get("/signup", toProfile, (req, res) => {
+  res.render("register", { title: "Signup" });
+});
+
+  router.post("/signup", toProfile, authcontrollers.signup);
+
+router.get("/login", toProfile, (req, res) => {
+  res.render("login", { title: "Login" });
+});
+
+router.post("/login", toProfile, authcontrollers.login);
+
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/login");
+});
+
+
+
+
+
+
+module.exports = router;
