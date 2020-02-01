9270a864375f2de58a5159ac1fc22f74fc6082d3
Edgar Chavero <edieveg316@gmail.com>
2020-02-01 11:42:26 -0600

    ruta auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,28 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { toProfile } = require("../config/middlewareAuth");
-const authcontrollers = require("../controllers/authController");
+cont User = require("../models/User");
+const passport = require("passport");
+const nodemailer = require("nodemailer");
 
-router.get("/signup", toProfile, (req, res) => {
-  res.render("register", { title: "Signup" });
+router.get("/login", (req, res) => {
+  res.render("auth-login", { action: "Login"});
 });
-
-  router.post("/signup", toProfile, authcontrollers.signup);
-
-router.get("/login", toProfile, (req, res) => {
-  res.render("login", { title: "Login" });
-});
-
-router.post("/login", toProfile, authcontrollers.login);
-
-router.get("/logout", (req, res) => {
-  req.logout();
-  res.redirect("/login");
-});
-
-
-
-
-
-
-module.exports = router;
