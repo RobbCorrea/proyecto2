c9957476b023ffb055cfc6aa3d9709c22caf7d9a
Edgar Chavero <edieveg316@gmail.com>
2020-02-01 14:27:04 -0600

    agregue mongodb

--- app.js
+++ app.js
@@ -12,7 +12,23 @@ const passport     = require("./config/passport");
 const connectDB    = require('./config/database');
 const hbs = require("hbs");
 
-connectDB();
+mongoose
+  .connect(process.env.DB || "mongodb://localhost/repaso-dos", {
+    useNewUrlParser: true
+  })
+  .then(x => {
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
+  })
+  .catch(err => {
+    console.error("Error connecting to mongo", err);
+  });
+
+const app_name = require("./package.json").name;
+const debug = require("debug")(
+  `${app_name}:${path.basename(__filename).split(".")[0]}`
+);
 
 const app = express();
 
