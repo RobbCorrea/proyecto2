ff9c02e6d18e14a21cf331a93a4d3d5d39985198
Edgar Chavero <edieveg316@gmail.com>
2020-02-05 10:58:48 -0600

    cambios en rutas

--- routes/auth.js
+++ routes/auth.js
@@ -1,96 +1,23 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/User");
-const passport = require("passport");
-const nodemailer = require("nodemailer");
+const { toProfile } = require("../config/middlewareAuth");
+const authcontrollers = require("../controllers/authcontrollers");
 
-router.get("/login", (req, res) => {
-  res.render("auth-login", { action: "Login"});
+router.get("/signup", toProfile, (req, res) => {
+  res.render("register", { title: "SignUp" });
 });
 
-router.get("/register", (req, res) => {
-  res.render("auth-login");
+  router.post("/signup", toProfile, authcontrollers.signup);
+
+router.get("/login", toProfile, (req, res ) => {
+  res.render("login", { title: "Login" });
 });
 
-router.post("/login", (req, res) => {
-  passport.authenticate("local", (err, user) => {
-    if(err) {
-      return res.render("auth-login", { err });
-    }
+router.post("/login", toProfile ,authcontrollers.login);
 
-    if(!user) {
-      return res.render("auth-login", { err});
-    }
-    req.logIn(user, err => {
-      if(err) {
-        return res.render("auth-login", { err });
-      }
-      if(user.status === "pending") {
-        return res.render("auth-login", {
-          pending: "Please verify your email",
-          action: "Login" 
-        });
-      }
-      return res.redirect("/profile");
-    });
-  })(req, res, next);
-});
-
-router.post("/register", (req, res) => {
-  const { password } = req.body;
-  const characters = 
-  "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ*+&$";
-  let token = "";
-  for (let i=0; i< 25; i++) {
-    token += characters[Math.floor(Math.random() * characters.length)];
-  }
-  const confirmationCode = token;
-
-  const user = { ...req.body, confirmationCode };
-
-  User.register(user, password)
-    .then(data => {
-      transporter.sendMail({
-        from: "HEYWERO<noreply@heywero.com>",
-        to: data.email,
-        subject: "Confirm your account werito at HEYWERO",
-
-        text: `Hello ${data.username}
-        Please wero dale click to confirm your HEYWERO account:
-        ${req.headers.origin}/auth/confirm/${data.confirmationCode}
-        Thanks!!.`
-      });
-        res.render("email-confirm", { data });
-      })
-      .catch(err => {
-        res.render("auth-login", { err });
-      });
-  });
-
-router.get("/confirm/:confirmCode", (req, res) => {
-  User.find({ confirmationCode: req.params.confirmationCode })
-  .then(user => {
-    let id = user[0]._id;
-
-    User.findByIdAndUpdate(id, { status: "confirmed" }, () => {
-      let userEmail = user[0].email;
-      let userId = user[0]._id;
-      res.render("confirm", { userEmail, userId });
-    });
-  });
-});
-
-router.get("/loguot", (req, res) => {
-  reg.logout();
-  res.redirect("/");
-});
-
-const transporter = nodemailer.createTransport({
-  service: "Gmail",
-  auth: {
-    user: process.env.MAILER_USER,
-    pass: process.env.MAILER_PASS
-  }
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/login");
 });
 
 module.exports = router;
--- routes/puesto.js
+++ routes/puesto.js
@@ -1,70 +1,55 @@
 const express = require("express");
 const router = express.Router();
+const { isAuth } = require("../config/middlewareAuth");
+const  uploader = require("../config/cloudinary");
 const Puesto = require("../models/Puesto");
-const uploader = require("../config/cloudinary");
 
-isAuth = (req, res, next) => {
-  if(req.isAuthenticated() && req.user.status === "confirm") {
-    next();
-  } else {
-    res.redirect("/auth/login");
-  }
-};
-
-router.get("/", (req, res) => {
+router.get("/new", isAuth, (req, res ) => {
   const { user } = req;
-  Puesto.find()
-  .limit(12)
-  .then(puestos => {
-    res.render("puestos", { puestos, user });
-  });
-});
 
-router.get("/json", (req, res) => {
-  Puesto.find()
-  .limit(14)
-  .then(puestos => {
-    res.status(200).json({ puestos});
-  });
-});
-
-router.get(":id/edit" , (req, res) => {
-  const { id} = req.params;
-  const { user } = req;
-  Puesto.findById(id).then(puesto => {
-    res.render("new-puesto", { user, puesto });
-  });
-});
-
-router.post("/:id/edit", isAuth, uploader.array("images"), (req, res) => {
-  const { id } = req.params;
-  const { lat, lng, address, ...puesto } = req.body;
-  let location = { address, coordinates: [lat, lng] };
-  Property.findByIdAndUpdate(id, { $set: { ...property, location } }).then(
-    () => {
-      res.redirect("/puestos");
-    }
-  );
-});
-
-router.get("/new", isAuth, (req, res) => {
-  const { user } = req;
-  res.render("new-puesto", { user });
+  res.render("newPuesto", { title: "New Puesto ", create: true, user });
 });
 
 router.post("/new", isAuth, uploader.array("images"), (req, res) => {
-  let images = req.files.map(file => file.url);
-  let { _id: owner } = req.user;
-  let { lat, lng, address, ...puesto } = req.body;
-  let location = { address, coordinates: [lat, lng] };
-  puesto = { ...puesto, images, owner, location };
-  Puesto.create(puesto)
-    .then(() => {
+  const { user } = req;
+  const {
+    user: { _id: owner }
+  } = req;
+
+  const { name, price, description } = req.body;
+  let images = req.files;
+  if(!name || !description || !images) {
+    let error = "Some fields are empty";
+    return res.render("newPuesto", {
+      title: "New Puesto",
+      create: true,
+      user,
+      error
+    });
+  } else {
+    images = req.files.map(file => file.secure_url);
+
+    const puesto = {
+      owner,
+      price,
+      name,
+      description,
+      images
+    };
+
+    console.log(puesto);
+
+    Puesto.create(puesto)
+    .then(newpuesto => {
       res.redirect("/profile");
     })
-    .catch(err => {
-      res.render("new-puesto", { err });
+    .catch(error => {
+      res.render("newPuesto", {
+        title: "New Puesto",
+        create: true,
+        user,
+        error
+      });
     });
+  }
 });
-
-module.exports = router;
