3c9457dfb1bea349cee4f5a5976afda54c2d755f
robbcorrea <51095085+RobbCorrea@users.noreply.github.com>
2020-02-08 11:24:46 -0600

    Hope

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const {connect} = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-fkeeh.mongodb.net/test?retryWrites=true&w=majority`, {useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false, useUnifiedTopology: true});
+        await connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-fkeeh.mongodb.net/test?retryWrites=true&w=majority`, {useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false, useUnifiedTopology: false});
         console.log('Base de datos conectada.');
     } catch(err) {
         console.error('Error en la base de datos', err);
